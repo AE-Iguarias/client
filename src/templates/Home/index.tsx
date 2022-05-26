297e6b52d5b8f41f55ecc2fcfdb6327308fc2ba5
padu <educartj@gmail.com>
2022-05-26 08:37:52 -0300

    Add initial template

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,10 @@
+import Base from 'templates/Base'
+import * as S from './styles'
+
+const Home = () => (
+  <Base>
+    <h1>Home</h1>
+  </Base>
+)
+
+export default Home
---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,4 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import * as HeadingStyles from 'components/Heading/styles'
